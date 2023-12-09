7b221c2117ea5985733184fa763c467d60313f49
YellowLime77 (Matthew Yang) <matthewyanglele@gmail.com>
2023-12-09 18:37:00 -0500

    Added UI

---
+++ Assets/Scripts/UpdateUI.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUI : MonoBehaviour
+{
+    private Dictionary<GameObject, string> uiObjects = new Dictionary<GameObject, string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+// Scoopula
+// StirringRod
+// Funnel
+// GraduatedCylinder
+// Goggles
+// MagneticStirBar
+// VialBase
+// WatchGlass
+// VolumetricFlaskGlass
+// TestTube
+// Pipette
+// BunsenBurner
+// Chair
+// ErenmyerFlask
+// Beaker (1)
+// CrucibleTongs
+// Centrifuge
+// RingStand
+
+// Scoopula
+// glass stir bar
+// Funnel
+// Graduated Cylinder
+// SAFETY GOGGLES
+// MAGNETIC STIR BAR
+// vial with cap
+// Watch Glass
+// Volumetric Flask
+// test tube
+// Pipet
+// BUNSEN BURNER
+// ***
+// erlenmeyer flask
+// Beaker
+// CRUCIBLE TONGS
+// CENTRIFUGE
+// Ring Stand
+
+        
+        uiObjects.Add(GameObject.Find("Scoopula"), "Scoopula");
+        uiObjects.Add(GameObject.Find("StirringRod"), "glass stir bar");
+        uiObjects.Add(GameObject.Find("Funnel"), "Funnel");
+        uiObjects.Add(GameObject.Find("GraduatedCylinder"), "Graduated Cylinder");
+        uiObjects.Add(GameObject.Find("Goggles"), "SAFETY GOGGLES");
+        uiObjects.Add(GameObject.Find("MagneticStirBar"), "MAGNETIC STIR BAR");
+        uiObjects.Add(GameObject.Find("VialBase"), "vial with cap");
+        uiObjects.Add(GameObject.Find("WatchGlass"), "Watch Glass");
+        uiObjects.Add(GameObject.Find("VolumetricFlaskGlass"), "Volumetric Flask");
+        uiObjects.Add(GameObject.Find("TestTube"), "test tube");
+        uiObjects.Add(GameObject.Find("Pipette"), "Pipet");
+        uiObjects.Add(GameObject.Find("BunsenBurner"), "BUNSEN BURNER");
+        uiObjects.Add(GameObject.Find("ErenmyerFlask"), "erlenmeyer flask");
+        uiObjects.Add(GameObject.Find("Beaker (1)"), "Beaker");
+        uiObjects.Add(GameObject.Find("CrucibleTongs"), "CRUCIBLE TONGS");
+        uiObjects.Add(GameObject.Find("Centrifuge"), "CENTRIFUGE");
+        uiObjects.Add(GameObject.Find("RingStand"), "Ring Stand");
+    }
+
+    public void UpdateImage(GameObject obj)
+    {
+        GameObject.Find("Image").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>(uiObjects[obj]);
+    }
+
+    public void RemoveImage()
+    {
+        GameObject.Find("Image").GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("transparent");
+    }
+}
